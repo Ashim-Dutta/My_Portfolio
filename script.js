5b133020c8edf5658929f28023acd74732d1bb25
ASHIM DUTTA <64308624+Ashim-Dutta@users.noreply.github.com>
2025-04-20 12:20:13 +0530

    Update script.js
    new animation

--- script.js
+++ script.js
@@ -99,5 +99,30 @@ document.querySelector("#about").addEventListener("click", function(e) {
 });
 
 
+window.addEventListener("wheel", function (dets) { 
+  if (dets.deltaY > 0) {
+    gsap.to(".marquee", {
+      transform: "translateX(-200%)",
+      repeat: -1,
+      duration: 4,
+      ease:"none"
+    })
 
+    gsap.to(".marquee img", {
+      rotate:180
+    })
+  }
+  else { 
+       gsap.to(".marquee", {
+      transform: "translateX(0%)",
+      repeat: -1,
+      duration: 4,
+      ease:"none"
+       })
+    
+     gsap.to(".marquee img", {
+      rotate:0
+    })
+  }
+})
   
